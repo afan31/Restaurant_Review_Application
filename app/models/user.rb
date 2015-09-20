937a4333ba02a573cd65751aec5861aa4b4ac6ac
Afan <afan.khan50@gmail.com>
2015-09-20 01:53:27 -0400

    Add data validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  validates :first_name, :last_name, presence: true
 end
